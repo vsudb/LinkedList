211c8dfd9c87c6c519c71f96591d59a4c7b991e7
fvvcpp <vsudb2025@mail.ru>
2026-05-18 21:43:42 +0300

    change LinkedList.cpp

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,239 +1,350 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
 
 //Узел
-
-typedef struct Node {   //  typedef позволяет в Си компиляторах 
-						//  использовать просто псевдоним Node вместо struct Node для улучшения читаемости и чистоты кода; 
-	int value;  // 4
-	struct Node* next; // 8
+typedef struct Node {
+    int value;  // 4
+    Node* next; // 8
 } Node;
 
+//  typedef позволяет в Си компиляторах
+//  использовать просто псевдоним Node вместо struct Node для улучшения читаемости и чистоты кода; 
+
 typedef struct LinkedList {
-	int size;
-	Node* head;
-	Node* tail;
+    int size;   // 4
+    Node* head; // 8
+    Node* tail; // 8
 } LinkedList;
 
-
-
-Node* createNode(int value);
-
-LinkedList* createList();
-
-LinkedList* createList(int arr[], int size);
+//==============Prototypes===============
 
 bool isEmpty(LinkedList* list);
 
+//инициализация Node
+Node* createNode(int val);
 
-void add(LinkedList* list, int value);
+//инициализация пустого LinkedList
+LinkedList* createList();
 
-void addFront(LinkedList* list, int value);
+//создвние списка заполненного данными из массива
+LinkedList* createList(int arr[], int size);
 
-void insertAt(LinkedList* list, int index, int value);
+//============== 3 function = 25 points ===============
 
 
-int get(LinkedList* list, int index);
+void add(LinkedList* list, int value); //append push_back вставка элемента в конец списка 
 
 void printList(LinkedList* list);
 
 void freeList(LinkedList* list);
 
+//===================================================
 
-// ================== БАЗА ==================
+//================ 2 function = 15 points ============
+
+
+LinkedList* insertAt(LinkedList* list, int index, int value);
+
+LinkedList* removeAt(LinkedList* list, int index);
+
+//===================================================
+
+
+int get(LinkedList* list, int index);
+
+void addFront(LinkedList* list, int value);
+
+void removeFront(LinkedList* list);
+
+void removeBack(LinkedList* list);
+
+//===================================================
+// На 45  баллов вам достанется одна из 3 задач
+//===================================================
+
+// 🔁 1. Разворот списка
+void reverse(LinkedList* list);
+
+// 🐢 2. Поиск цикла(Floyd)
+bool hasCycle(LinkedList* list);
+
+//🧹 3. Удаление дубликатов  (НЕ отсортированный — O(n²))
+void removeDuplicatesUnsorted(LinkedList* list);
+
+//===================================================
+// На 50  баллов           продвинутый уровень Floyd 
+//===================================================
+//
+
+//🧠 Задача : Найти начало цикла в связном списке
+//             Дан односвязный список.
+//             Некоторые узлы могут образовывать цикл — то есть последний элемент списка может указывать на один из предыдущих.
+//   Ваша задача :
+//              🔍 Определить, есть ли цикл в списке, и если есть — вернуть узел, с которого начинается цикл.
+//                 Если цикла нет — вернуть NULL.
+
+Node* detectCycleStart(LinkedList* list);
+
+//===================================================
 
 bool isEmpty(LinkedList* list) {
-	return (list == NULL || list->size == 0);
+    return list == NULL || list->size == 0;
 }
 
-Node* createNode(int value) {
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->value = value;
-	node->next = NULL;
-	return node;
-}
+Node* createNode(int val) {
+    Node* node = (Node*)malloc(sizeof(Node));
 
-// ➕ В начало (O(1))
-void addFront(LinkedList* list, int value) {
-	Node* node = createNode(value);
+    if (!node) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
 
-	if (isEmpty(list)) {
-		list->head = list->tail = node;
-	}
-	else {
-		node->next = list->head;
-		list->head = node;
-	}
-	list->size++;
-}
+    node->next = NULL; //*(node).next;
+    node->value = val;
 
-
-// ➕ В конец (O(1))
-void add(LinkedList* list, int value) {
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->value = value;
-	node->next = NULL;
-
-	if (isEmpty(list)) {
-		list->head = node;
-		list->tail = node;
-	}
-	else {
-		list->tail->next = node;
-		list->tail = node;
-	}
-	list->size++;
-}
-
-// ➕ В середину (по индексу)
-void insertAt(LinkedList* list, int index, int value) {
-	if (index < 0 || index > list->size) return;
-
-	if (index == 0) {
-		addFront(list, value);
-		return;
-	}
-
-	if (index == list->size) {
-		add(list, value);
-		return;
-	}
-
-	Node* node = createNode(value);
-	Node* cur = list->head;
-
-	for (int i = 0; i < index - 1; i++) {
-		cur = cur->next;
-	}
-
-	node->next = cur->next;
-	cur->next = node;
-
-	list->size++;
-}
-
-
-
-// ================== УДАЛЕНИЕ ==================
-
-void removeAt(LinkedList* list, int index) {
-	if (isEmpty(list) || index < 0 || index >= list->size) return;
-
-	Node* toDelete;
-
-	// удаление головы
-	if (index == 0) {
-		toDelete = list->head;
-		list->head = list->head->next;
-
-		if (list->size == 1)
-			list->tail = NULL;
-	}
-	else {
-		Node* cur = list->head;
-
-		for (int i = 0; i < index - 1; i++) {
-			cur = cur->next;
-		}
-
-		toDelete = cur->next;
-		cur->next = toDelete->next;
-
-		if (toDelete == list->tail)
-			list->tail = cur;
-	}
-
-	free(toDelete);
-	list->size--;
-}
-
-
-// ================== ДОСТУП ==================
-
-int get(LinkedList* list, int index) {
-	if (isEmpty(list) || index < 0 || index >= list->size) {
-		printf("Index out of bounds\n");
-		return -1;
-	}
-
-	Node* cur = list->head;
-
-	for (int i = 0; i < index; i++) {
-		cur = cur->next;
-	}
-
-	return cur->value;
-}
-
-// ================== СЛУЖЕБНЫЕ ==================
-
-LinkedList* createList(int arr[], int size) {
-	LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
-	if (list == NULL) {
-		return NULL;
-	}
-
-	list->head = NULL;
-	list->tail = NULL;
-	list->size = 0;
-
-	for (int i = 0; i < size; i++) {
-		add(list, arr[i]);
-	}
-	return list;
+    return node;
 }
 
 LinkedList* createList() {
-	LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
-	list->head = NULL;
-	list->tail = NULL;
-	list->size = 0;
-	return list;
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
+
+    if (!list) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+
+    return list;
+}
+
+LinkedList* createList(int arr[], int size) {
+    LinkedList* list = createList();
+
+    for (int i = 0; i < size; i++) {
+        add(list, arr[i]);
+    }
+    return list;
 }
 
 
+void add(LinkedList* list, int value) { //push_back
+    Node* node = createNode(value);
+
+    if (isEmpty(list)) {
+        list->head = node;
+        list->tail = node;
+
+        //list->head = list->tail = node;
+    }
+    else {
+        list->tail->next = node;
+        list->tail = node;
+
+        //list->tail = list->tail->next = node;
+    }
+    list->size++;
+}
+
 void printList(LinkedList* list) {
-	
-	if (isEmpty(list)) {
-		printf("List is empty\n");
-		return;
-	}
+    if (!isEmpty(list)) {
+        Node* cur = list->head;
+        printf("==============list==============\n");
 
-	Node* cur = list->head;
-	for (int i = 0; i < list->size; i++) {
-		printf("%d ", cur->value);
-		cur = cur->next;
-	}
+        while (cur != NULL) {
+            printf("%d ", cur->value);
+            cur = cur->next;
+        }
+        printf("\n");
 
-	printf("\n");
+        printf("================================\n");
+        printf("head->value = %d \n", list->head->value);
+        printf("tail->value = %d \n", list->tail->value);
+        printf("list->size  = %d \n", list->size);
+        printf("================================\n");
+
+    }
+    else {
+        printf("================================\n");
+        printf("List is Empty \n");
+        printf("================================\n");
+
+    }
 }
 
 void freeList(LinkedList* list) {
-	if (list != NULL) {
+    Node* cur = list->head;
 
-		Node* cur = list->head;
+    while (cur != NULL) {
+        Node* next = cur->next;
+        free(cur);
+        cur = next;
+    }
 
-		while (cur != NULL) {
-			Node* next = cur->next;
-			free(cur);
-			cur = next;
-		}
-		free(list);
-	}
+    free(list);
 }
+
+
+void addFront(LinkedList* list, int value) {
+    Node* node = createNode(value);
+
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+}
+
+void removeFront(LinkedList* list) {
+
+    if (isEmpty(list)) {
+        return;
+    }
+
+    Node* deleted = list->head;
+    list->head = list->head->next;
+    free(deleted);
+    list->size--;
+
+    if (list->size == 0) {
+        list->tail = NULL;
+    }
+}
+
+void removeBack(LinkedList* list) {
+    if (isEmpty(list)) {
+        return;
+    }
+
+    if (list->size == 1) {
+        free(list->head);
+        list->head = NULL;
+        list->tail = NULL;
+        list->size = 0;
+        return;
+    }
+
+    Node* cur = list->head;
+    while (cur->next != list->tail) {
+        cur = cur->next;
+    }
+
+    free(list->tail);
+    list->tail = cur;
+    list->tail->next = NULL;
+    list->size--;
+}
+
+
+LinkedList* insertAt(LinkedList* list, int index, int value) {
+
+    printList(list);
+    printf("insertAt index = [%d] \n", index);
+
+    if (isEmpty(list) || index < 0 || index > list->size) {
+        printf("Index = [%d] Out Of Bound Exception\n", index);
+        return list;
+    }
+
+    if (index == 0) {
+        addFront(list, value);
+    }
+    else if (index == list->size) {
+        add(list, value);
+    }
+    else {
+        Node* node = createNode(value);
+        Node* cur = list->head;
+
+        for (int i = 0; i < index - 1; i++) {
+            cur = cur->next;
+        }
+
+        node->next = cur->next;
+        cur->next = node;
+
+        list->size++;
+    }
+
+    printList(list);
+
+    return list;
+}
+
+LinkedList* removeAt(LinkedList* list, int index) {
+
+
+    printList(list);
+    printf("removeAt index = [%d] \n", index);
+
+    if (isEmpty(list) || index < 0 || index >= list->size) {
+        printf("Index [%d] out of bounds\n", index);
+        return list;
+    }
+
+    if (index == 0) {
+        removeFront(list);
+    }
+    else if (index == list->size - 1) {
+        removeBack(list);
+    }
+    else {
+
+        Node* cur = list->head;
+        for (int i = 0; i < index - 1; i++) {
+            cur = cur->next;
+        }
+
+        Node* deleted = cur->next;
+        cur->next = deleted->next;
+        free(deleted);
+
+        list->size--;
+    }
+
+    printList(list);
+
+
+    return list;
+
+}
+
+
+
+int get(LinkedList* list, int index) {
+
+    if (index < 0 || index >= list->size) {
+        printf("Index [%d] out of bounds\n", index);
+        return -1;
+    }
+
+    Node* cur = list->head;
+    for (int i = 0; i < index; i++) {
+        cur = cur->next;
+    }
+
+    return cur->value;
+}
+
 
 int main()
 {
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
 
-	LinkedList* list  = createList(arr, sizeof(arr) / sizeof(int));
-	printf("%d \n", get(list, 5));
+    LinkedList* list = createList(arr, sizeof(arr) / sizeof(int));
+    printList(list);
 
-	
-	printList(list);
-	freeList(list);
+    //insertAt(list, 4, 444);
+    //insertAt(list, 0, 111);
+    //insertAt(list, 9, 999);
+    //insertAt(list, 10, 1010);
+    //insertAt(list, 11, 1111);
+
+    //removeAt(list, 4);
+    //removeAt(list, 0);
+    //removeAt(list, list->size);
+    //removeAt(list, list->size - 1);
+
+
+    freeList(list);
 
 }
-
